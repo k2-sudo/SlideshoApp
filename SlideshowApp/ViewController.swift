3d013dade2686bb05009c2ea6126ada54fc60240
Kazuhiro Sudo <kazuhiro@kazuhiros-macbook-pro.local>
2016-04-06 16:46:23 +0800

    updated

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,13 +13,12 @@ class ViewController: UIViewController {
     var imageList :Array <UIImage> = []
     var counter = 0
     var isPlaying = false
+    let filenames = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg",]
     
     @IBOutlet var backbutton: UIButton!
-    
     @IBOutlet var playstopbutton: UIButton!
-    
     @IBOutlet var nextbutton: UIButton!
-    
+    @IBOutlet var imageArea: UIImageView!
 
     @IBAction func backAction(sender: AnyObject) {
         if counter == 0 {
@@ -31,7 +30,6 @@ class ViewController: UIViewController {
         //print("Counter: " + (String)(counter))
     }
     
-    
     @IBAction func nextAction(sender: AnyObject) {
         if counter == imageList.count - 1 {
             counter = 0
@@ -42,13 +40,11 @@ class ViewController: UIViewController {
         //print("Counter: " + (String)(counter))
     }
     
-    
     @IBAction func playStopAction(sender: AnyObject) {
-        
         if isPlaying{//再生中にボタンを押すと停止する
             
             
-
+            
             playstopbutton.setTitle("再生", forState: .Normal)
             isPlaying = false
         }else{//停止中にボタンを押すと再生が始まる
@@ -60,33 +56,32 @@ class ViewController: UIViewController {
         
     }
     
-    
-    @IBOutlet var imageArea: UIImageView!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let image0:UIImage! = UIImage (named: "0.jpg")
-        let image1:UIImage! = UIImage (named: "1.jpg")
-        let image2:UIImage! = UIImage (named: "2.jpg")
-        let image3:UIImage! = UIImage (named: "3.jpg")
-        //let image4:UIImage! = UIImage (named: "4.jpg")
+        imageArea.userInteractionEnabled = true
+        imageArea.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "imageTapped:"))
 
-        imageList.append(image0)
-        imageList.append(image1)
-        imageList.append(image2)
-        imageList.append(image3)
-
+        print("# of images: " + (String)(filenames.count))
+        
+        var i = 0
+        while i < filenames.count{
+            let imagetmp:UIImage! = UIImage (named: filenames[i])
+            imageList.append(imagetmp)
+            i++
+        }
         imageArea.image = imageList[0]
-        print("Array length: " + (String)(imageList.count))
-        
-        //imageArea.image = UIImage(named: "images/3.jpg")
-        //imageArea.image = UIImage(named: "4.jpg")
-        
-        
-        
+    }
+    
+    func imageTapped(sender: UITapGestureRecognizer) {
+        print("Filename: " + filenames[counter])
+
+        //let targetView: ResultViewController? = self.storyboard!.instantiateViewControllerWithIdentifier( "DetailedImage" ) as? ResultViewController
+        //self.presentViewController( targetView as! UIViewController, animated: true, completion: nil)
+        //self.presentViewController( targetView!, animated: true, completion: nil)
+        let targetView: ResultViewController = self.storyboard!.instantiateViewControllerWithIdentifier( "DetailedImage" ) as! ResultViewController
+        self.presentViewController( targetView, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
