f8b399979104908408de49fff486ac2bebc575f4
Kazuhiro Sudo <kazuhiro@kazuhiros-macbook-pro.local>
2016-04-07 16:34:51 +0800

    final version

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,9 +12,15 @@ class ViewController: UIViewController {
 
     var imageList :Array <UIImage> = []
     var counter = 0
-    var isPlaying = false
+    var interval = 2.0 //再生間隔
     let filenames = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg",]
+
+    var isPlaying = false
+    var wasPlayed = false
+    var timer = NSTimer()
     
+    
+    //@IBOutlet var backbutton: UIButton!
     @IBOutlet var backbutton: UIButton!
     @IBOutlet var playstopbutton: UIButton!
     @IBOutlet var nextbutton: UIButton!
@@ -42,19 +48,29 @@ class ViewController: UIViewController {
     
     @IBAction func playStopAction(sender: AnyObject) {
         if isPlaying{//再生中にボタンを押すと停止する
-            
-            
-            
-            playstopbutton.setTitle("再生", forState: .Normal)
-            isPlaying = false
+            stopSlide()
         }else{//停止中にボタンを押すと再生が始まる
-            
-            
-            playstopbutton.setTitle("停止", forState: .Normal)
-            isPlaying = true
+            playSlide()
         }
         
     }
+    
+    func playSlide(){
+        timer = NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+        isPlaying = true
+        playstopbutton.setTitle("停止", forState: .Normal)
+        nextbutton.enabled = false
+        backbutton.enabled = false
+    }
+    
+    func stopSlide(){
+        timer.invalidate()
+        isPlaying = false
+        playstopbutton.setTitle("再生", forState: .Normal)
+        nextbutton.enabled = true
+        backbutton.enabled = true
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,15 +88,40 @@ class ViewController: UIViewController {
             i++
         }
         imageArea.image = imageList[0]
+        
+    }
+    
+    func doAnimation() {
+        if counter == imageList.count - 1 {
+            counter = 0
+        } else {
+            counter++
+        }
+        imageArea.image = imageList[counter]
+        print("Filename: " + filenames[counter])
     }
     
     func imageTapped(sender: UITapGestureRecognizer) {
-        print("Filename: " + filenames[counter])
-
-        //let targetView: ResultViewController? = self.storyboard!.instantiateViewControllerWithIdentifier( "DetailedImage" ) as? ResultViewController
-        //self.presentViewController( targetView as! UIViewController, animated: true, completion: nil)
+        //print("Filename: " + filenames[counter])
         let targetView: ResultViewController = self.storyboard!.instantiateViewControllerWithIdentifier( "DetailedImage" ) as! ResultViewController
+        if isPlaying{
+            stopSlide()
+            wasPlayed = true
+        }
+        targetView.passedFilename = filenames[counter]
+        targetView.passedImage = imageList [counter]
         self.presentViewController( targetView, animated: true, completion: nil)
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        //initialize when back from the detailed image
+        print("Welcome Back!")
+        if wasPlayed{
+            playSlide()
+            wasPlayed = false
+        }
+        //text1.text = ""
+        //errorlabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
